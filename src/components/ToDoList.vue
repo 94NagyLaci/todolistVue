2c5b82066c716c2203102376c4b6a79403e4a96b
Nagy László <sze1naglas@vasvari.org>
2022-12-05 17:10:05 +0100

    todok elkészítése

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,9 +1,24 @@
-<template lang="">
-  <div>
-    
-  </div>
+<template>
+
+  <input type="text" v-model='todo' @change="addItem(todo)" placeholder="Új feladat megadása">
+
+  <ul>
+    <li v-for="needtodo in todoList">
+    {{needtodo}}
+    </li>
+  </ul>
+
 </template>
 <script setup>
+
+import {ref} from 'vue';
+const todo = ref('');
+const todoList = ref([]);
+
+function addItem(t){
+  todoList.value.push(t);
+  todo.value = '';
+}
 
 </script>
 <style scoped>
